578674d796f7f9e80eeaf9d20df50dde4b179b3f
Liane Chan <liane.chan@gmail.com>
2018-05-24 15:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function logShout
 function whisper(string) {
   return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if string
 }
