5b5530f60f0a4a835f888ee85a99cc6260aca696
Liane Chan <liane.chan@gmail.com>
2018-05-24 15:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function sayHiToGrandma(string) {
   else if (string === uppercase) {
     return "YES INDEED!"
   }
-  else if (string = "I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
