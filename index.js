2704248dc0bf236c2c754dae62235106a50db560
Liane Chan <liane.chan@gmail.com>
2018-05-24 15:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase  // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase // false
+
 function sayHiToGrandma(string) {
-  if string
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+  else if (string = "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
